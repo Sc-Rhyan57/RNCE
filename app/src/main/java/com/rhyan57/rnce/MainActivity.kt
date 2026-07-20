a283d8d20e8cfdd21180b202fc6dca582d9a7593
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-07-20 09:47:52 -0300

    Implement crash handler in MainActivity
    Added crash handling functionality to MainActivity.

--- app/src/main/java/com/rhyan57/rnce/MainActivity.kt
+++ app/src/main/java/com/rhyan57/rnce/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.rhyan57.rnce
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.rhyan57.rnce.hooks.MainViewModel
+import com.rhyan57.rnce.ui.components.CrashScreen
 import com.rhyan57.rnce.ui.screens.MainScreen
 import com.rhyan57.rnce.ui.theme.AppTheme
+import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
 
@@ -17,17 +18,52 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        vm = ViewModelProvider(this)[MainViewModel::class.java]
+
+        setupCrashHandler()
+
+        val showCrash = intent.getBooleanExtra("show_crash", false)
+
+        if (!showCrash) {
+            vm = ViewModelProvider(this)[MainViewModel::class.java]
+        }
+
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                MainScreen(vm = vm)
+                if (showCrash && CrashHandler.trace != null) {
+                    val trace = CrashHandler.trace!!
+                    CrashHandler.trace = null
+                    CrashScreen(trace = trace)
+                } else {
+                    MainScreen(vm = vm)
+                }
             }
+        }
+    }
+
+    private fun setupCrashHandler() {
+        Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+            CrashHandler.trace = throwable.stackTraceToString()
+            
+            val intent = Intent(this, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                putExtra("show_crash", true)
+            }
+            startActivity(intent)
+            
+            android.os.Process.killProcess(android.os.Process.myPid())
+            exitProcess(10)
         }
     }
 
     override fun onResume() {
         super.onResume()
-        vm.refreshNfcState()
+        if (this::vm.isInitialized) {
+            vm.refreshNfcState()
+        }
     }
+}
+
+object CrashHandler {
+    var trace: String? = null
 }
